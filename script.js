09b656ad2d6ff329c325fff7eb5298409858c538
BorgXQ <jonathan.widarsa@gmail.com>
2023-05-11 23:55:35 +0800

    Add transition time to toggle

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+const time = "0.4s";
 var icon = document.getElementById("moon");
 icon.onclick = function() {
     document.body.classList.toggle("darkTheme");
     if (document.body.classList.contains("darkTheme")) {
         icon.name = "sunny-outline";
-    }
-else icon.name = "moon-outline";
-}
+    } else icon.name = "moon-outline";
+
+    icon.style.transitionDuration = time;
+};
