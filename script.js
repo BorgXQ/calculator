d24f2011caea71d844963bad2ff626e7e1f318f9
BorgXQ <jonathan.widarsa@gmail.com>
2023-05-11 23:30:04 +0800

    Fix light/dark mode button

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function changeIcon() {
-    document.body.classList.toggle("darkTheme");
-    if (document.body.classList.contains("darkTheme")) {
-        icon.name = "sunny-outline";
-    }
+window.onload = function() {
+    var icon = document.getElementById("moon");
+    icon.onclick = function() {
+        document.body.classList.toggle("darkTheme");
+        if (document.body.classList.contains("darkTheme")) {
+            icon.name = "sunny-outline";
+        }
     else icon.name = "moon-outline";
+    }
 }
-
-var icon = document.getElementById("sun");
-icon.onclick = changeIcon()
