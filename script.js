0d4d4c10df2e4b06e4034f55fd6b9c34e53946e7
BorgXQ <jonathan.widarsa@gmail.com>
2023-05-11 23:32:16 +0800

    Remove window.onload

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-window.onload = function() {
-    var icon = document.getElementById("moon");
-    icon.onclick = function() {
-        document.body.classList.toggle("darkTheme");
-        if (document.body.classList.contains("darkTheme")) {
-            icon.name = "sunny-outline";
-        }
-    else icon.name = "moon-outline";
+var icon = document.getElementById("moon");
+icon.onclick = function() {
+    document.body.classList.toggle("darkTheme");
+    if (document.body.classList.contains("darkTheme")) {
+        icon.name = "sunny-outline";
     }
+else icon.name = "moon-outline";
 }
