067bb4cf62d225bca7bd77f079056f12b4f4cbf6
BorgXQ <jonathan.widarsa@gmail.com>
2023-05-11 23:15:12 +0800

    Add changeIcon() function

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-var icon = document.getElementById("sun");
-icon.onclick = function() {
+function changeIcon() {
     document.body.classList.toggle("darkTheme");
     if (document.body.classList.contains("darkTheme")) {
         icon.name = "sunny-outline";
     }
     else icon.name = "moon-outline";
 }
+
+var icon = document.getElementById("sun");
+icon.onclick = changeIcon()
